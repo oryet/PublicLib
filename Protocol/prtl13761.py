e1691b67976fc0a304b1ec5ad5e9e0fd92d25c6e
oryet <jerry_jzy@126.com>
2019-05-23 11:11:45 +0800

    增加组帧函数

--- Protocol/prtl13761.py
+++ Protocol/prtl13761.py
@@ -14,7 +14,7 @@ class prtl3761():
     def Fun2Data(self, fun):
         d2 = fun / 8
         d1 = fun % 8
-        return d1, d2
+        return int(d1), int(d2)
 
     def deal3761Frame(self, frame):
         for i in range(0, len(frame), 2):
@@ -39,11 +39,41 @@ class prtl3761():
                 return True
         return False
 
-    def make3761Frame(self):
-        pass
+    def frameAFN00(self, fn):
+        self.frame['AFN'] = '00'
+        dt1, dt2 = self.Fun2Data(fn)
+        self.frame['DT1'] = hex(dt1).replace('0x','00')[-2:]
+        self.frame['DT2'] = hex(dt2).replace('0x','00')[-2:]
 
+    def make3761Frame(self, du):
+        self.frame['ctrl'] = '40'
+        datastr = self.frame['ctrl'] + self.frame['A1'] + self.frame['A2'] + self.frame['A3']
+        datastr += self.frame['AFN'] + self.frame['SEQ']
+        datastr += self.frame['DA1'] + self.frame['DA2']
+        datastr += self.frame['DT1'] + self.frame['DT2']
+        datastr += du # 数据单元
+
+        self.frame['crc'] = calcCheckSum(datastr)
+
+        datalen = int(len(datastr)/2)
+        datalenstr = hex(datalen<<2).replace('0x','00')[-2:] + '00'
+        self.frame['len'] = datalenstr[-4:]
+
+        frame = '68' + self.frame['len'] + self.frame['len'] + '68' + datastr + self.frame['crc'] + '16'
+        return frame
+
+    def LoginHeartFrame(self, frame):
+        if self.deal3761Frame(frame):
+            if self.frame['AFN'] == "02" and (self.frame['Fn'] == 0 or self.frame['Fn'] == 3):
+                self.frameAFN00(0)
+                return self.make3761Frame(du='')
+        return None
 
 if __name__ == '__main__':
     frame = u"683200320068c95507070000027100000100a016"
     p = prtl3761()
     p.deal3761Frame(frame)
+
+    p.frameAFN00(0)
+    f = p.make3761Frame(du='')
+    print(f)
