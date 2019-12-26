b928612bd6955e7ab29944dfaf14c9fd8a37d763
jerryGithub <jerry_jzy@126.com>
2019-12-26 14:01:39 +0800

    删除需量的变比

--- ACModule/read_demand.py
+++ ACModule/read_demand.py
@@ -1,13 +1,12 @@
 import PublicLib.Protocol.dl645format as fat
 
-CT = 1000
 
 def dl645_readdemand(DI, eng, pn=3):
     # demand  [相位][类型][0]
     e = []
 
     if DI[1] == 0x80 and DI[2] == 0x00 and DI[3] == 0x03:  # 一分钟有功总平均功率
-        e = [eng[0][0][0] / CT]
+        e = [eng[0][0][0]]
 
     strdata = ''
     for i in range(len(e)):
