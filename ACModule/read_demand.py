367a37e8652665ec8f12393148d54305cde5bfa6
jerryGithub <jerry_jzy@126.com>
2021-10-14 17:23:38 +0800

    增加当前需量

--- ACModule/read_demand.py
+++ ACModule/read_demand.py
@@ -7,6 +7,12 @@ def dl645_readdemand(DI, eng, pn=3):
 
     if DI[1] == 0x80 and DI[2] == 0x00 and DI[3] == 0x03:  # 一分钟有功总平均功率
         e = [eng[0][0][0]]
+    elif DI[1] == 0x80 and DI[2] == 0x00 and DI[3] == 0x04:  # 当前有功需量
+        e = [eng[0][0][0]]
+    elif DI[1] == 0x80 and DI[2] == 0x00 and DI[3] == 0x05:  # 当前无功需量
+        e = [eng[0][1][0]]
+    elif DI[1] == 0x80 and DI[2] == 0x00 and DI[3] == 0x06:  # 当前视在需量
+        e = [(eng[0][0][0]**2 + eng[0][1][0]**2) ** 0.5]
 
     strdata = ''
     for i in range(len(e)):
